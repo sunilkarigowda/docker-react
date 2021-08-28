1dfd8332513cc240eb0ba80485ab46b035a76869
Sunil Karigowda <sunimysooru@gmail.com>
2021-08-28 15:24:27 +0200

    blah

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there!
+          Bye there1!
         </p>
       </header>
     </div>
