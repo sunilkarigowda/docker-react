bc7fbc953ccb75414acf08a64e3abae99bb50747
Sunil Karigowda <sunimysooru@gmail.com>
2021-08-28 16:13:09 +0200

    fix tests

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there1!
+          Learn React
         </p>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText("learn react");
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Learn React')).toBeInTheDocument();
 });
